2706b6f37a44323393d4a2ddb46dac486379ffde
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-08-01 15:31:40 +0900

    feat : 상단 네비게이션 바에 API 선택하는 select 추가

--- src/components/features/Layout/Header.tsx
+++ src/components/features/Layout/Header.tsx
@@ -4,10 +4,30 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Container } from '@/components/common/layouts/Container';
 import { useAuth } from '@/provider/Auth';
 import { getDynamicPath, RouterPath } from '@/routes/path';
+import { useState } from 'react';
+import { useServer } from '@/provider/ServerProvider';
 
 export const Header = () => {
   const navigate = useNavigate();
   const authInfo = useAuth();
+  const { serverURL, setServerURL } = useServer();
+
+  const servers = [
+    { name: '김송목', baseURL: 'https://api.example.com' },
+    { name: '이강욱', baseURL: 'https://api.example2.com' },
+    { name: '이의찬', baseURL: 'https://api.example3.com' },
+    { name: '전혜지', baseURL: 'https://api.example4.com' },
+  ];
+
+  const [selectedServer, setSelectedServer] = useState(
+    serverURL || servers[0].baseURL
+  );
+
+  const handleServerChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedURL = event.target.value;
+    setSelectedServer(selectedURL);
+    setServerURL(selectedURL); // 전역적으로 사용하는 서버 URL 설정 업데이트
+  };
 
   const handleLogin = () => {
     navigate(getDynamicPath.login());
@@ -15,16 +35,29 @@ export const Header = () => {
 
   return (
     <Wrapper>
-      <Container flexDirection="row" alignItems="center" justifyContent="space-between">
+      <Container
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='space-between'
+      >
         <Link to={RouterPath.home}>
           <Logo
-            src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png"
-            alt="카카오 선물하기 로고"
+            src='https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png'
+            alt='카카오 선물하기 로고'
           />
         </Link>
         <RightWrapper>
+          <select onChange={handleServerChange} value={selectedServer}>
+            {servers.map((server) => (
+              <option key={server.baseURL} value={server.baseURL}>
+                {server.name}
+              </option>
+            ))}
+          </select>
           {authInfo ? (
-            <LinkButton onClick={() => navigate(RouterPath.myAccount)}>내 계정</LinkButton>
+            <LinkButton onClick={() => navigate(RouterPath.myAccount)}>
+              내 계정
+            </LinkButton>
           ) : (
             <LinkButton onClick={handleLogin}>로그인</LinkButton>
           )}
@@ -49,7 +82,9 @@ export const Wrapper = styled.header`
 const Logo = styled.img`
   height: ${HEADER_HEIGHT};
 `;
-const RightWrapper = styled.div``;
+const RightWrapper = styled.div`
+  display: flex;
+`;
 
 const LinkButton = styled.p`
   align-items: center;
@@ -57,4 +92,5 @@ const LinkButton = styled.p`
   color: #000;
   text-decoration: none;
   cursor: pointer;
+  margin-left: 20px;
 `;
