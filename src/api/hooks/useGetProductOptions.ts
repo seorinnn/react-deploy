d6122f022c1d3ce781010729741db210113a1ab5
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-08-01 20:13:12 +0900

    feat : 선택한 서버 주소로 상품 옵션 조회하도록 변경

--- src/api/hooks/useGetProductOptions.ts
+++ src/api/hooks/useGetProductOptions.ts
@@ -1,27 +1,32 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-
+import { useServer } from '@/provider/ServerProvider';
 import type { ProductOptionsData } from '@/types';
 
-import { BASE_URL, fetchInstance } from '../instance';
+import { fetchInstance } from '../instance';
 import type { ProductDetailRequestParams } from './useGetProductDetail';
 
 type Props = ProductDetailRequestParams;
 
 export type ProductOptionsResponseData = ProductOptionsData[];
 
-export const getProductOptionsPath = (productId: string) =>
-  `${BASE_URL}/api/products/${productId}/options`;
+export const getProductOptionsPath = (serverURL: string, productId: string) =>
+  `${serverURL}/api/products/${productId}/options`;
 
-export const getProductOptions = async (params: ProductDetailRequestParams) => {
+export const getProductOptions = async (
+  serverURL: string,
+  params: ProductDetailRequestParams
+) => {
   const response = await fetchInstance.get<ProductOptionsResponseData>(
-    getProductOptionsPath(params.productId),
+    getProductOptionsPath(serverURL, params.productId)
   );
   return response.data;
 };
 
 export const useGetProductOptions = ({ productId }: Props) => {
+  const { serverURL } = useServer();
+
   return useSuspenseQuery({
-    queryKey: [getProductOptionsPath(productId)],
-    queryFn: () => getProductOptions({ productId }),
+    queryKey: [serverURL, 'product-options', productId],
+    queryFn: () => getProductOptions(serverURL, { productId }),
   });
 };
